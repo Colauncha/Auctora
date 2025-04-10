15d3c644877dad215a63d80d0fa0ea56bd44119c
Iyanu Ajimobi <iyanuajimobi12@gmail.com>
2025-04-10 02:26:47 +0100

    Make bid amount optional in CreateBidSchema with default value of 0.0

--- Backend/server/schemas/bid_schema.py
+++ Backend/server/schemas/bid_schema.py
@@ -8,7 +8,7 @@ class CreateBidSchema(BaseModel):
     auction_id: Union[UUID, str] = Field(example=uuid4(), description="Auction ID")
     user_id: Optional[UUID] = Field(default=None,example=uuid4(), description="user ID")
     username: Optional[str] = Field(default=None, example=app_configs.test_user.USERNAME)
-    amount: float = Field(example=100.00, description="Amount to place on bid")
+    amount: Optional[float] = Field(default=0.0, description="Amount to place on bid")
     model_config = {'from_attributes': True}
 
 
