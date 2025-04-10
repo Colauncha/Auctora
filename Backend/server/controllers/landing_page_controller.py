f318f3819feedf2a00a84ca9eda6c669812b8b5a
Iyanu Ajimobi <iyanuajimobi12@gmail.com>
2025-04-11 00:28:16 +0100

    Implement Redis caching for trending auctions in landing page controller

--- Backend/server/controllers/landing_page_controller.py
+++ Backend/server/controllers/landing_page_controller.py
@@ -1,7 +1,8 @@
+import json
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from server.config import app_configs, get_db
+from server.config import app_configs, get_db, redis_store
 from server.services.auction_service import AuctionServices
 from server.schemas import PagedQuery
 
@@ -11,12 +12,24 @@ router = APIRouter(prefix='/landing', tags=['Landing Page'])
 
 @router.get('/trending_auctions')
 async def get_trending_auctions(db: Session = Depends(get_db)):
-    filter = PagedQuery(page=1, per_page=10)
-    auctions = await AuctionServices(db).list(
-        filter,
-        {'status': 'ACTIVE'}
-    )
-    return auctions
+    redis = await redis_store.get_async_redis()
+    trending_auctions = await redis.get('trending_auctions')
+    if trending_auctions:
+        return json.loads(trending_auctions)
+    # If not in Redis, fetch from DB
+    else:
+        filter = PagedQuery(page=1, per_page=10)
+        auctions = await AuctionServices(db).list(
+            filter,
+            {'status': 'ACTIVE'}
+        )
+        # Store in Redis for future requests
+        await redis.set(
+            'trending_auctions',
+            json.dumps(auctions),
+            ex=app_configs.REDIS_CACHE_EXPIRATION_LANDING
+        )
+        return auctions
 
 @router.get('/search')
 async def search(db: Session = Depends(get_db)):
